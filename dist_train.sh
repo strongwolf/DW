d4be265be1341dda0d3bbd3a4ec7cae53a92787e
strongwolf <742580713@qq.com>
2022-03-18 17:27:07 +0800

    Update training code

---
+++ dist_train.sh
@@ -0,0 +1,7 @@
+CONFIG=$1
+GPUS=$2
+PORT=${PORT:-29500}
+
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+    det6/train.py $CONFIG --launcher pytorch ${@:3}
