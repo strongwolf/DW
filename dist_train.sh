e6d295afc17fcea438d4e1a3deba2198a4906a1d
strongwolf <742580713@qq.com>
2022-03-20 13:29:24 +0800

    Update dist_train.sh

--- dist_train.sh
+++ dist_train.sh
@@ -4,4 +4,4 @@ PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    det6/train.py $CONFIG --launcher pytorch ${@:3}
+    train.py $CONFIG --launcher pytorch ${@:3}
